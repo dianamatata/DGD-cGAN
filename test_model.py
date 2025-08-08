e038a40effdaeccd965551e1088e33034ddedae8
dianamatata <diana.avalos.m@gmail.com>
2025-08-08 15:21:33 +0200

    device gpu cpu mps + paths

--- test_model.py
+++ test_model.py
@@ -13,7 +13,15 @@ from torchvision.utils import save_image
 from torch.autograd import Variable
 from nets.networks_Dgd import ResUnet1, ResUnet2, PatchDiscriminator
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+if torch.cuda.is_available():
+    print("CUDA is available. Running on GPU.")
+    device = torch.device("gpu")
+elif torch.backends.mps.is_available():
+    print("CUDA is not available. Running on MPS.")
+    device = torch.device("mps")
+else:
+    print("CUDA and MPS are not available. Running on CPU.")
+    device = torch.device("cpu")
 
 G1 = ResUnet1().to(device)
 G2 = ResUnet2().to(device)
@@ -22,25 +30,25 @@ D = PatchDiscriminator(in_c=3, num_filters=64, n_down=3).to(device)
 
 # Download the model weights https://drive.google.com/file/d/11fJ4WrxLCWIF890PvaHp_EuriKwI6QWl/view?usp=share_link
 #Load state dic
-G1.load_state_dict(torch.load(f'G_dgdgan_epoch_850.pth', map_location=device))
-G2.load_state_dict(torch.load(f'G2_dgdgan_epoch_850.pth', map_location=device))
-D.load_state_dict(torch.load(f'D_dgdgan_epoch_850.pth', map_location=device))
+G1.load_state_dict(torch.load(f'weights/G_dgdgan_epoch_850.pth', map_location=device))
+G2.load_state_dict(torch.load(f'weights/G2_dgdgan_epoch_850.pth', map_location=device))
+D.load_state_dict(torch.load(f'weights/D_dgdgan_epoch_850.pth', map_location=device))
 
 
 #Input path
-input_path = f"/your/path"
-os.chdir(input_path)
-img_folder = os.listdir(input_path)
+input_path = '/Users/avalos/Documents/Programming/DATA_Rovailake/material'
+img_folder = glob.glob(os.path.join(input_path, '*.jpg'))
+
 
 #Output path
-output_dir = f"/your/path"
+output_dir = f"/Users/avalos/Documents/Programming/DATA_Rovailake/underwater_image_restoration/DGD-cGAN"
 
 #Mode eval
 G1.eval()
 D.eval()
 
 #Data transform
-transform = transforms.Compose([transforms.Resize((256, 256), transforms.InterpolationMode.BICUBIC)])
+transform = transforms.Compose([transforms.Resize((1024, 1024), transforms.InterpolationMode.BICUBIC)])
 
 
 ## testing 
@@ -48,13 +56,14 @@ count = []
 d=1
 
 for img in img_folder:
-    img_test = transform(Image.open(img))
+    print(img)
+    img_test = transform(Image.open(f"{img}"))
     img_test = transforms.ToTensor()(img_test)
     input_img = img_test.unsqueeze_(0)
     input_img = input_img.to(device)
     dewatered_img = G1(input_img)
     dewatered_sample = dewatered_img
-    image_name = (img.split('.')[-2] +'_%d.jpg'%d)
+    image_name = (img.split('/')[-1][:-4] +'_%d.jpg'%d)
     file_path = os.path.join(output_dir, image_name)
     save_image(dewatered_sample, file_path, normalize=True)
     d+=1
